bae8460b796f053363c5c5f4e658a0bf642ce426
daniel landesman <daniellandesman@daniels-MacBook-Pro.local>
2020-01-19 22:28:46 -0500

    new file#

---
+++ email_messages.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 20:19:26 2019
+
+@author: daniellandesman
+"""
+
+subject = "Shabbat {}"
+
+msg_minyan_details = '''
+Minyan details for this week:
+Candle lighting {}
+Mincha Friday: {}
+Mincha Shabbat: {} 
+Shabbat Ends: {}
+
+@ 13 Eagles Notch, at the home of Mr. and Mrs. Douek 
+'''
+
+msg_signup = '''
+YOUR_GOOGLE_DOC
+'''
+msg_signoff = '''
+P.S. This email is auto-generated. If you notice something wrong, please feel free to reach out.
+
+-ShulPresidente
+'''
+
+msg_current_tally = '''
+Current Tally:
+    Friday: {} 
+    Saturday: {}
+'''
+
+#msg_monday = msg_minyan_details + msg_signup + msg_signoff
+#
+#msg_thursday = msg_minyan_details + msg_current_tally + msg_signoff
+msg_welcome_default = '''
+All, please welcome to Cross Creek Minyan
+'''
+msg_welcome = '''
+All, please welcome to Cross Creek Minyan
+'''
+
+msg_current_tally = '''
+Current Tally:
+    Friday: {} 
+    Saturday: {}
+'''
+
+msg_need_ppl_fri = '''
+Only need {} more for Friday night.
+'''
+
+msg_need_ppl_sat = '''
+Only need {} more for Shabbos Mincha. 
+'''
+msg_post_script = '''
+P.S. This email is auto-generated. If you notice something wrong, please feel
+free to reach out.
+
+-ShulPresidente
+'''
+
+msg_fri_minyan_confirmed = '''
+All,
+
+Glad to see a computer can do this job better than me. We are on for 
+Friday night.
+'''
+
+msg_sat_minyan_confirmed = '''
+We are on for Shabbos Mincha
+'''
+msg_both_confirmed = '''
+We are on for both!
+'''
+
+msg_fri_nogo = '''
+Friday night is a no go. We'll try again next week.
+'''
+
+msg_sat_nogo = '''
+Shabbos Mincha is a no go.
+'''
+msg_both_nogo = '''
+Minyan is a no go this week. We'll try again next week.
+'''
+
+msg_will_confirm = '''
+Will confirm status of both Minyanim by Friday afternoon the latest.
+'''
