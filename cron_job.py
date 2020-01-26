3fe55f4526df02217328a9a1cccee6e735701c6f
Sam Lightstone <slightstone@gmail.com>
2020-01-25 21:24:05 -0500

    updated test cron job and added it to poetry

--- cron_job.py
+++ cron_job.py
@@ -9,14 +9,14 @@ Created on Sun Jan 12 19:49:10 2020
 from crontab import CronTab
 
 #my_cron = CronTab(user='daniellandesman')
-my_cron = CronTab(user = 'daniellandesman')
+my_cron = CronTab(user=True)
 for job in my_cron:
     print(job)
     
 #job = my_cron.new(command='/Applications/anaconda3/bin/python /Users/daniellandesman/Documents/Python_scripts/personal/manage_minyan/on_monday.py')
-job = my_cron.new(command = 'python test.py')
-#job.dow.on('SUN')
-#job.hour.also.on(20)
-#job.minute.also.on(19)
+job = my_cron.new(command='cd scripts/personal/manage_minyan; poetry run python test.py')
+job.dow.on('SAT')
+job.hour.on(21)
+job.minute.on(7)
 job.minute.every(1)
 my_cron.write()
